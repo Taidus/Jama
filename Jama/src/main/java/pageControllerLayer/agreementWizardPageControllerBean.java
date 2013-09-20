4637c60f448e078a950111600a81bb78561fc164
Taidus <taidus@hotmail.it>
2013-09-20 18:47:23 +0200

    Wizard WIP

--- Jama/src/main/java/pageControllerLayer/agreementWizardPageControllerBean.java
+++ Jama/src/main/java/pageControllerLayer/agreementWizardPageControllerBean.java
@@ -6,11 +6,13 @@ import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.enterprise.context.Conversation;
 import javax.enterprise.context.ConversationScoped;
+import javax.faces.model.SelectItem;
 import javax.inject.Inject;
 import javax.inject.Named;
 
 import businessLayer.Agreement;
 import businessLayer.AgreementShareTable;
+import businessLayer.AgreementType;
 import daoLayer.AgreementDaoBean;
 
 @Named("agreementWizardPCB")
@@ -45,7 +47,7 @@ public class agreementWizardPageControllerBean implements Serializable {
 	
 	public AgreementShareTable getAgreementShareTable() {
 		return agreementShareTable;
-	}	
+	}
 		
 	public Conversation getConversation() {
 		return conversation;
--- Jama/src/main/java/presentationLayer/AgreementWizardPresentationBean.java
+++ Jama/src/main/java/presentationLayer/AgreementWizardPresentationBean.java
@@ -3,7 +3,12 @@ package presentationLayer;
 import java.io.Serializable;
 
 import javax.enterprise.context.SessionScoped;
+import javax.faces.model.SelectItem;
 import javax.inject.Named;
+
+import businessLayer.AgreementType;
+import businessLayer.ChiefScientist;
+import businessLayer.Company;
 
 @Named("agreementWizPB")
 @SessionScoped
@@ -12,15 +17,41 @@ public class AgreementWizardPresentationBean implements Serializable{
 	 */
 	private static final long serialVersionUID = 1L;
 	
-	private String currentChief;
-	
-	
-	public String getCurrentChief() {
-		return currentChief;
+	public SelectItem[] getAgreementTypeItems(){
+		AgreementType[] types = AgreementType.values();
+		return arrayToSelectItems(types);
 	}
-
-	public void setCurrentChief(String currentChief) {
-		this.currentChief = currentChief;
+	
+	public SelectItem[] getChiefItems(){
+		//FIXME initialize properly
+		ChiefScientist[] chiefs = new ChiefScientist[2];
+		chiefs[0] = new ChiefScientist();
+		chiefs[0].setName("Beppe");
+		chiefs[0].setSurname("Modica");
+		chiefs[1] = new ChiefScientist();
+		chiefs[1].setName("Enrico");
+		chiefs[1].setSurname("Vicario");
+		
+		return arrayToSelectItems(chiefs);
+	}
+	
+	public SelectItem[] getCompanyItems(){
+		//FIXME initialize properly
+		Company[] companies = new Company[2];
+		companies[0] = new Company();
+		companies[0].setName("Apple");
+		companies[1] = new Company();
+		companies[1].setName("Microsoft");
+		
+		return arrayToSelectItems(companies);
+	}
+	
+	private <T> SelectItem[] arrayToSelectItems(T[] array){
+		SelectItem[] result = new SelectItem[array.length];
+		for(int i=0; i< array.length; i++){
+			result[i] = new SelectItem(array[i], array[i].toString());
+		}
+		return result;
 	}
 	
 	public AgreementWizardPresentationBean() {}
