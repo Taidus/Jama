56a1bb254b4f9cda82ff07af6783b1483630f6cc
Taidus <taidus@hotmail.it>
2013-10-23 13:31:17 +0200

    rework AgreementListPageController e LazyModel + scadenzario

---
+++ Jama/src/main/java/controllerLayer/AgreementSchedulePageControllerBean.java
@@ -0,0 +1,30 @@
+package controllerLayer;
+
+import java.io.Serializable;
+
+import javax.enterprise.context.ConversationScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import presentationLayer.LazyAgreementScheduleDataModel;
+
+@Named("agreementSchedulePCB")
+@ConversationScoped
+public class AgreementSchedulePageControllerBean extends AgreementTablePageController implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Inject
+	private LazyAgreementScheduleDataModel lazyModel;
+
+	@Override
+	public LazyAgreementScheduleDataModel getLazyModel() {
+		return lazyModel;
+	}
+
+	public String viewAgreement() {
+		lazyModel.filterOnReload();
+		agrManager.setSelectedAgreementId(lazyModel.getSelectedValue().getId());
+		return agrManager.viewAgreement();
+	}
+
+}
---
+++ Jama/src/main/java/controllerLayer/AgreementTablePageController.java
@@ -0,0 +1,40 @@
+package controllerLayer;
+
+import java.io.Serializable;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.Conversation;
+import javax.enterprise.context.ConversationScoped;
+import javax.inject.Inject;
+
+import presentationLayer.LazyAgreementDataModel;
+
+@ConversationScoped
+public abstract class AgreementTablePageController implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@Inject protected AgreementManagerBean agrManager;
+	@Inject protected Conversation conversation;
+
+	@PostConstruct
+	public void init() {
+		conversation.begin();
+	}
+
+	public Conversation getConversation() {
+		return conversation;
+	}
+
+	public abstract LazyAgreementDataModel getLazyModel();
+
+	protected void close() {
+		conversation.end();
+	}
+
+	public String backToHome() {
+		close();
+		return "/home?faces-redirect=true";
+	}
+
+
+}
