dbf939003e976546e5d87bec8b598eef7452d29e
Taidus <taidus@hotmail.it>
2013-10-24 20:04:20 +0200

    prototipo mailSender

--- Jama/src/main/java/util/Config.java
+++ Jama/src/main/java/util/Config.java
@@ -1,12 +1,50 @@
 package util;
 
+import java.io.File;
+import java.io.IOException;
+
+import freemarker.template.Configuration;
+import freemarker.template.DefaultObjectWrapper;
+import freemarker.template.TemplateExceptionHandler;
+import freemarker.template.Version;
+
 
 public class Config {
 
 	public static int defaultPageSize = 30;
 
-	//private static final String filePath = "/usr/local/share/jboss/standalone/deployments/Jama.war/WEB-INF/classes/";
-	public static final String fileDir = "../standalone/deployments/Jama.war/WEB-INF/classes";
+	private static final String fileDir = "../standalone/deployments/Jama.war/WEB-INF/classes";
 	public static final String depRatesPath = fileDir + "/" + "aliquoteDipartimenti/";
+	public static final Configuration fmconf;
+	public static final String mailTemplateFileName = "mailTemplate.ftl";
 	
+	
+	static{
+		fmconf = new Configuration();
+
+		// Specify the data source where the template files come from. Here I set a
+		// plain directory for it, but non-file-system are possible too:
+		try {
+			fmconf.setDirectoryForTemplateLoading(new File(Config.fileDir));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		// Specify how templates will see the data-model. This is an advanced topic...
+		// for now just use this:
+		fmconf.setObjectWrapper(new DefaultObjectWrapper());
+
+		// Set your preferred charset template files are stored in. UTF-8 is
+		// a good choice in most applications:
+		fmconf.setDefaultEncoding("UTF-8");
+
+		// Sets how errors will appear. Here we assume we are developing HTML pages.
+		// For production systems TemplateExceptionHandler.RETHROW_HANDLER is better.
+		fmconf.setTemplateExceptionHandler(TemplateExceptionHandler.HTML_DEBUG_HANDLER);
+
+		// At least in new projects, specify that you want the fixes that aren't
+		// 100% backward compatible too (these are very low-risk changes as far as the
+		// 1st and 2nd version number remains):
+		fmconf.setIncompatibleImprovements(new Version(2, 3, 20));  // FreeMarker 2.3.20
+	}
 }
--- Jama/src/main/java/util/MailSender.java
+++ Jama/src/main/java/util/MailSender.java
@@ -1,10 +1,13 @@
 package util;
 
+import java.io.IOException;
 import java.io.Serializable;
+import java.io.StringWriter;
 
 import javax.annotation.Resource;
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Inject;
+import javax.inject.Named;
 import javax.mail.MessagingException;
 import javax.mail.Session;
 import javax.mail.Transport;
@@ -12,9 +15,14 @@ import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 import javax.mail.internet.MimeMessage.RecipientType;
 
-import annotations.Logged;
 import usersManagement.User;
+import annotations.Logged;
+import businessLayer.Agreement;
+import businessLayer.Installment;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
 
+@Named
 @SessionScoped
 public class MailSender implements Serializable {
 
@@ -30,19 +38,19 @@ public class MailSender implements Serializable {
 	@Logged
 	private User loggedUser;
 
-	public void sendToLoggedUser(String text,String subject){
-		
+	private void sendToLoggedUser(String text, String subject) {
+		// XXX eliminare?
+
 		String recipientEmail = loggedUser.getEmail();
-		send(recipientEmail, subject, text);
-		
+		_send(recipientEmail, subject, text);
+
 	}
 
-	private void send(String recipientEmail, String subject, String text) {
+	private void _send(String recipientEmail, String subject, String text) {
 
 		MimeMessage message = new MimeMessage(mailSession);
 		try {
-			message.setRecipient(RecipientType.TO, new InternetAddress(
-					recipientEmail));
+			message.setRecipient(RecipientType.TO, new InternetAddress(recipientEmail));
 			message.setSubject(subject);
 			message.setText(text);
 			message.saveChanges();
@@ -55,19 +63,69 @@ public class MailSender implements Serializable {
 
 	}
 
-	public void spam() {
-		MimeMessage message = new MimeMessage(mailSession);
-		try {
-			message.setRecipient(RecipientType.TO, new InternetAddress(
-					"tommaso.levato@gmail.com"));
-			message.setSubject("Promozione");
-			message.setText("Sei Stato promosso al grado di sergente nella DeltaSpikeForce");
-			message.saveChanges();
+	private void spam() {
+		// XXX inutile ai fini della business logic, ma chi non vorrebbe mandare
+		// spam a Damaz?
+		_send("damaz91@live.it", "Promozione", "Sei Stato promosso al grado di sergente nella DeltaSpikeForce!");
+	}
 
-			Transport.send(message);
-		} catch (MessagingException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+	public void send(Installment inst) throws IOException, TemplateException {
+		
+		TemplateFiller filler = new TemplateFiller(inst, "pluto@jama.jam", "topolino@jama.jam");
+		StringWriter out = new StringWriter();
+		// variabile di tipo StringWriter perché un Writer qualunque non va
+		// bene: serve che il metodo toString() restituisca esattamente la
+		// stringa che rappresenta il contenuto della mail
+		
+		Template temp = Config.fmconf.getTemplate(Config.mailTemplateFileName);
+		temp.process(filler, out);
+		String mailContent = out.toString();
+
+		//System.out.println("\n********************\n" + mailContent + "\n*********************");
+		_send("giulio.galvan@stud.unifi.it", "Comunicazione da Jama", mailContent);
+		_send("tommaso.levato@stud.unifi.it", "Comunicazione da Jama", mailContent);
+		_send("alessio.sarullo@stud.unifi.it", "Comunicazione da Jama", mailContent);
+		spam();
+	}
+
+	public static class TemplateFiller {
+		private Agreement agreement;
+		private Installment installment;
+		private String mail1, mail2;
+		private Integer installmentNumber;
+
+		public TemplateFiller(Installment installment, String mail1, String mail2) {
+			super();
+			this.agreement = installment.getAgreement();
+			this.installment = installment;
+			this.mail1 = mail1;
+			this.mail2 = mail2;
+			this.installmentNumber = 1+ agreement.getInstallments().indexOf(installment);
+
+			if (installmentNumber <= 0) {
+				throw new IllegalStateException("Disallineamento tra la rata " + installment.getId() + " e la convenzione " + agreement.getId()
+						+ " (la convenzione non ha un riferimento alla rata)");
+			}
+		}
+
+		public Agreement getAgreement() {
+			return agreement;
+		}
+
+		public Installment getInstallment() {
+			return installment;
+		}
+
+		public Integer getInstallmentNumber() {
+			return installmentNumber;
+		}
+
+		public String getMail1() {
+			return mail1;
+		}
+
+		public String getMail2() {
+			return mail2;
 		}
 
 	}
