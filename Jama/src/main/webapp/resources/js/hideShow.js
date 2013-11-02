c1c044bcfab66050b34417a28d1511994148ac91
Taidus <taidus@hotmail.it>
2013-11-02 16:35:28 +0100

    fix vari

--- Jama/src/main/webapp/resources/js/hideShow.js
+++ Jama/src/main/webapp/resources/js/hideShow.js
@@ -1,17 +1,17 @@
-function setHideShow(){
-	
+function setHideShow() {
 	$("div[class*=hideShow] tr").find(".list-row-button").hide();
 
 	$("div[class*=hideShow] tr").mouseover(function() {
-			jQuery(this).find(".list-row-button").show();
-		}).mouseout(function() {
-			jQuery(this).find(".list-row-button").hide();
-		}).click(function(e) {
-			jQuery("div[class*=contextMenu]").hide();
-			jQuery("div[class*=hideShow] tr").removeClass("ui-state-highlight");
-			e.stopPropagation();
-		}).removeClass("ui-state-highlight");
-	
-	
-	
+		jQuery(this).find(".list-row-button").show();
+	}).mouseout(function() {
+		jQuery(this).find(".list-row-button").hide();
+	}).click(function(e) {
+		jQuery("div[class*=contextMenu]").hide();
+		jQuery("div[class*=hideShow] tr").removeClass("ui-state-highlight");
+		e.stopPropagation();
+	}).removeClass("ui-state-highlight");
 }
+
+$(document).ready(function() {
+	setHideShow();
+});
--- Jama/src/main/webapp/resources/js/onEnterPressInWiz.js
+++ Jama/src/main/webapp/resources/js/onEnterPressInWiz.js
@@ -1,7 +1,3 @@
-/**
- * 
- */
-
 function enterNext() {
 	alert("sssss");
 
