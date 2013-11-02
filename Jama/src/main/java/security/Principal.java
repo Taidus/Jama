bdf50c1ff6a4a4624b2839a33cc10c90407cc094
tommasolevato <tommaso.levato@gmail.com>
2013-11-02 17:09:57 +0100

    ruolo Principal

--- Jama/src/main/java/security/Principal.java
+++ Jama/src/main/java/security/Principal.java
@@ -8,27 +8,33 @@ import usersManagement.Role;
 
 public class Principal {
 	private String serialNumber;
-
+	private Role role;
 	private List<String> permissions = new ArrayList<>();
 
 	public Principal() {
 		// TODO: di serialNumber che ci metto?
 		serialNumber = "Guest";
+		role = Role.GUEST;
 		// FIXME: rimettere GUEST quando sarà tutto montato
 		setPermissions(Role.ADMIN.getPermissions());
 	}
 
 	public Principal(String serialNumber, Role role) {
 		this.serialNumber = serialNumber;
+		this.role = role;
 		setPermissions(role.getPermissions());
 	}
 
 	public String getSerialNumber() {
 		return serialNumber;
 	}
-	
+
 	public boolean hasPermission(Permission toCheck) {
 		return permissions.contains(toCheck.toString());
+	}
+
+	public boolean hasRole(Role toCheck) {
+		return role.equals(toCheck);
 	}
 
 	private void setPermissions(List<Permission> permissions) {
