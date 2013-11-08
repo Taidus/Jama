4d73c7cef7b183603836944ce05e38566c8b4c5e
GiulioGx <giulio.galvan@gmail.com>
2013-11-08 18:53:14 +0100

    security method annotations

--- Jama/src/main/java/usersManagement/Role.java
+++ Jama/src/main/java/usersManagement/Role.java
@@ -8,19 +8,21 @@ public enum Role {
 	// stato ancora definito al momento della creazione delle enum constants e
 	// quindi non si possono prendere dal message bundle
 
-	OPERATOR(new Permission[] { Permission.ALTER_CONTRACTS, Permission.DELETE_CONTRACTS, Permission.VIEW_CONTRACTS, Permission.VIEW_HOME },
-			"Operatore"),
+	OPERATOR(new Permission[] { Permission.ALTER_CONTRACTS,
+			Permission.DELETE_CONTRACTS, Permission.VIEW_CONTRACTS,
+			Permission.VIEW_HOME, Permission.VIEW_OWN_CONTRACTS }, "Operatore"),
 
-	CHIEF_SCIENTIST(new Permission[] { Permission.VIEW_OWN_CONTRACTS, Permission.VIEW_HOME }, "Responsabile scientifico"),
+	CHIEF_SCIENTIST(new Permission[] { Permission.VIEW_OWN_CONTRACTS,
+			Permission.VIEW_HOME }, "Responsabile scientifico"),
 
-	ADMIN(new Permission[] { Permission.VIEW_HOME, Permission.ALTER_USER_PERMISSIONS, Permission.CREATE_USER, Permission.VIEW_USERS },
-			"Amministratore"),
+	ADMIN(new Permission[] { Permission.VIEW_HOME,
+			Permission.ALTER_USER_PERMISSIONS, Permission.CREATE_USER,
+			Permission.VIEW_USERS }, "Amministratore"),
 
 	GUEST(new Permission[] {}, "Guest");
 
 	private List<Permission> permissions;
 	private String displayString;
-
 
 	private Role(Permission[] permissions, String displayString) {
 		this.displayString = displayString;
@@ -31,21 +33,17 @@ public enum Role {
 		}
 	}
 
-
 	public String getDisplayString() {
 		return displayString;
 	}
-
 
 	public boolean hasPermission(Permission toCheck) {
 		return permissions.contains(toCheck);
 	}
 
-
 	public List<Permission> getPermissions() {
 		return permissions;
 	}
-
 
 	public static Role[] getAvailableUserRoleValues() {
 		return (new Role[] { Role.CHIEF_SCIENTIST, Role.OPERATOR });
