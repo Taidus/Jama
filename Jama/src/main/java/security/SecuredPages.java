5dfd88bce7082a7f880e774bf9f8d67e00955539
tommasolevato <tommaso.levato@gmail.com>
2013-11-08 15:16:16 +0100

    refactor sicurezza parte 2

--- Jama/src/main/java/security/SecuredPages.java
+++ Jama/src/main/java/security/SecuredPages.java
@@ -3,32 +3,36 @@ package security;
 import org.apache.deltaspike.core.api.config.view.ViewConfig;
 import org.apache.deltaspike.security.api.authorization.Secured;
 
-@Secured(value = { OperatorAccessDecisionVoter.class }, errorView = Login.class)
+@Secured(value = { OperatorAccessDecisionVoter.class }, errorView = NotAuthorized.class)
 class AgreementEdit implements ViewConfig {
 }
 
-@Secured(value = { OperatorAccessDecisionVoter.class }, errorView = Login.class)
+@Secured(value = { OperatorAccessDecisionVoter.class }, errorView = NotAuthorized.class)
 class AgreementWiz implements ViewConfig {
 }
 
-@Secured(value = { OperatorAccessDecisionVoter.class }, errorView = Login.class)
+@Secured(value = { OperatorAccessDecisionVoter.class }, errorView = NotAuthorized.class)
 class InstallmentEdit implements ViewConfig {
 }
 
-@Secured(value = { OperatorAccessDecisionVoter.class }, errorView = Login.class)
+@Secured(value = { OperatorAccessDecisionVoter.class }, errorView = NotAuthorized.class)
 class AgreementList implements ViewConfig {
 }
 
-@Secured(value = { OperatorAccessDecisionVoter.class }, errorView = Login.class)
+@Secured(value = { OperatorAccessDecisionVoter.class }, errorView = NotAuthorized.class)
 class InstallmentWiz implements ViewConfig {
 }
 
 @Secured(value = { HomeAccessDecisionVoter.class }, errorView = Login.class)
-class Home implements ViewConfig {}
+class Home implements ViewConfig {
+}
 
-@Secured(value = { OperatorAccessDecisionVoter.class }, errorView = Login.class)
+@Secured(value = { OperatorAccessDecisionVoter.class }, errorView = NotAuthorized.class)
 class AgreementSchedule implements ViewConfig {
 }
 
+class NotAuthorized implements ViewConfig {
+}
 
-class Login implements ViewConfig {}
+class Login implements ViewConfig {
+}
