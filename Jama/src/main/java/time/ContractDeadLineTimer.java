c547986a75e38caac484d5038c87b4bb94be6049
Taidus <taidus@hotmail.it>
2013-11-15 18:46:47 +0100

    cattura eccezioni mail + fix vari

--- Jama/src/main/java/time/ContractDeadLineTimer.java
+++ Jama/src/main/java/time/ContractDeadLineTimer.java
@@ -1,6 +1,5 @@
 package time;
 
-import java.io.IOException;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.TimerTask;
@@ -15,11 +14,9 @@ import javax.persistence.TypedQuery;
 
 import util.MailSender;
 import businessLayer.Installment;
-import freemarker.template.TemplateException;
 
 @Stateful
 @ApplicationScoped
-
 public class ContractDeadLineTimer extends TimerTask {
 
 	@PersistenceContext(unitName = "primary", type = PersistenceContextType.EXTENDED)
@@ -29,42 +26,37 @@ public class ContractDeadLineTimer extends TimerTask {
 
 	private Calendar date;
 
+
 	public ContractDeadLineTimer() {
 		super();
 	}
+
 
 	public Calendar getDate() {
 		return date;
 	}
 
+
 	public void setDate(Calendar date) {
 		this.date = date;
 	}
 
+
 	@Override
 	public void run() {
-		
+
 		Calendar date = Calendar.getInstance();
-		System.err.println(new Date(date.getTimeInMillis())+"\n=========\n"+"Avvio task periodico di notifica scadenze\n"+"==========");
+		System.err.println(new Date(date.getTimeInMillis()) + "\n=========\n" + "Avvio task periodico di notifica scadenze\n" + "==========");
 
 		if (date != null) {
 
-			TypedQuery<Installment> query = em.createNamedQuery(
-					"Installment.findInstallmentsWithNearDeadLine",Installment.class).setParameter(
+			TypedQuery<Installment> query = em.createNamedQuery("Installment.findInstallmentsWithNearDeadLine", Installment.class).setParameter(
 					"date", new Date(date.getTimeInMillis()));
-			 
-			
-			for(Installment i : query.getResultList()){
-				
-				try {
-					mailSender.notifyDeadline(i);
-				} catch (IOException | TemplateException e) {
-					e.printStackTrace();
-				}
-			}
-			
 
-			
+			for (Installment i : query.getResultList()) {
+				mailSender.notifyDeadline(i);
+			}
+
 		}
 	}
 
