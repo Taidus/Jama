223b648d36b216e839513106b2ae513863d7bc3c
Taidus <taidus@hotmail.it>
2013-11-17 12:18:45 +0100

    filtro utenti

--- Jama/src/main/java/presentationLayer/lazyModel/UserTableLazyDataModel.java
+++ Jama/src/main/java/presentationLayer/lazyModel/UserTableLazyDataModel.java
@@ -3,48 +3,44 @@ package presentationLayer.lazyModel;
 import java.util.List;
 import java.util.Map;
 
-import javax.annotation.PostConstruct;
 import javax.enterprise.context.Dependent;
 import javax.inject.Inject;
 
 import org.primefaces.model.LazyDataModel;
 import org.primefaces.model.SortOrder;
 
-import daoLayer.UserSearchService;
 import usersManagement.User;
+import daoLayer.UserSearchService;
 
 @Dependent
 public class UserTableLazyDataModel extends LazyDataModel<User> {
 	private static final long serialVersionUID = 1719783501989512734L;
-	
+
 	@Inject
 	private UserSearchService searchService;
-	
+
 	private List<User> displayedUsers;
-	
-	@PostConstruct
-	public void init(){
-		searchService.init();
-		setRowCount(searchService.getResultNumber().intValue());
-	}
-	
+
+
 	@Override
 	public List<User> load(int first, int pageSize, String sortField, SortOrder sortOrder, Map<String, String> filters) {
-		
+
 		String filter = filters.get("surname");
 		System.out.println("User filter: " + filter);
-		
-		//TODO passare
-		
+
+		searchService.init(filter);
+		setRowCount(searchService.getResultNumber().intValue());
+
 		searchService.setCurrentPage(first / pageSize);
 		searchService.setPageSize(pageSize);
-		
+
 		displayedUsers = searchService.getCurrentResults();
-		
+
 		return displayedUsers;
 	}
-	
-	public void closePager(){
+
+
+	public void closePager() {
 		searchService.finished();
 	}
 
