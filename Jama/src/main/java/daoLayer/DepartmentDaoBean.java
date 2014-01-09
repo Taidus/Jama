6dca1452dd77a0e84c16b73c7c014e99542d7ee6
GiulioGx <giulio.galvan@gmail.com>
2014-01-09 15:24:43 +0100

    fix cecchi

--- Jama/src/main/java/daoLayer/DepartmentDaoBean.java
+++ Jama/src/main/java/daoLayer/DepartmentDaoBean.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.ejb.Stateful;
 import javax.enterprise.context.ConversationScoped;
 import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
 import javax.persistence.PersistenceContextType;
 
@@ -14,7 +15,7 @@ import businessLayer.Department;
 @ConversationScoped
 public class DepartmentDaoBean {
 
-	@PersistenceContext(unitName = "primary",type=PersistenceContextType.EXTENDED)
+	@PersistenceContext(unitName = "primary", type = PersistenceContextType.EXTENDED)
 	private EntityManager em;
 
 	public DepartmentDaoBean() {
@@ -35,23 +36,28 @@ public class DepartmentDaoBean {
 		}
 
 	}
-	
-	
-	public Department getById(int id){
-		
-		return em.find(Department.class,id);
+
+	public Department getById(int id) {
+
+		return em.find(Department.class, id);
 	}
-	
-	public Department getByCode(String code){
-		System.out.println("GET BY CODE: "+code +"   ////");
-		return em.createNamedQuery("Department.findByCode", Department.class).setParameter("code", code).getSingleResult();
+
+	public Department getByCode(String code) {
+		System.out.println("GET BY CODE: " + code + "   ////");
+		try {
+			return em
+					.createNamedQuery("Department.findByCode", Department.class)
+					.setParameter("code", code).getSingleResult();
+		} catch (NoResultException e) {
+			return null;
+		}
 	}
-	
-	
-	public List<Department> getAll(){
-		
-		return em.createNamedQuery("Department.findAll",Department.class).getResultList();
-		
+
+	public List<Department> getAll() {
+
+		return em.createNamedQuery("Department.findAll", Department.class)
+				.getResultList();
+
 	}
 
 }
